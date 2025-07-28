db8c6ff6de0d39ce419a4edb41707b6a151e5394
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-27 22:48:11 -0500

    feat: configure navbar sites

---
+++ src/shared/site.ts
@@ -0,0 +1,33 @@
+export const siteConfig = {
+  name: "PECO",
+  description:
+    "PECO fomentando la unión de la tecnología con el mar.",
+  navItems: [
+    {
+      label: "Funcionalidad 0",
+      href: "/f0",
+    },
+    {
+      label: "Funcionalidad 1",
+      href: "/f1",
+    },
+    {
+      label: "Conocenos",
+      href: "/conocenos",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Funcionalidad 0",
+      href: "/f0",
+    },
+    {
+      label: "Funcionalidad 1",
+      href: "/f1",
+    },
+    {
+      label: "Conocenos",
+      href: "/conocenos",
+    },
+  ],
+};
